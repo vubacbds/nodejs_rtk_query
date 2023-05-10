1480d854431a7af4f68ac5c46677a47568b15dde
Vu Xuan Bac <vubacbds@gmail.com>
2023-05-10 09:58:35 +0700

    test loi 3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ db.connect();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL ?? "http://localhost:300099",
+    origin: process.env.CLIENT_URL ?? "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
